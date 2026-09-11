57d908da2c384235195ca58675eb89dcdd4a983b
P4tr1c1o-M <a01712044@tec.mx>
2026-09-10 20:49:52 -0600

    Update GameFinder.py

--- GameFinder.py
+++ GameFinder.py
@@ -1,12 +1,15 @@
 def datos():
-    Gen=input("¿Qué género te gustaria probar? ")
-    Plataforma= input("¿En qué plataforma juegas? ")
+    Gen=input("¿Qué género te gustaria probar? (Acción, Aventura, RPG, Mundo Abierto, Deportes o Simulación) ")
+    Plataforma= input("¿Juegas en PlayStation, Nintendo, Xbox o PC? ")
     Dificultad= input("¿Qué nivel de dificultad te gustaría? ¿Facil, Medio, Dificil? ")
     Duracion= int(input("¿Aproximadamente cuantó tiempo quieres dedicarle (horas)? "))
-    if Duracion<0 or Duracion>200:
+    Sem=int(input("¿En cuantas semanas quieres terminarlo? "))
+    if (Duracion<0 or Duracion>800):
         Duracion=int(input("Dame un valor correcto para la duración: "))
     else:
         print("Duración valida:", Duracion, "horas")
+        Horasxsemana=Duracion/Sem
+        print("Tendrías que jugar aproximadamente", Horasxsemana, "horas por semana")
     
     
     return (Gen, Plataforma, Dificultad, Duracion)
